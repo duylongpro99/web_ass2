9e6d141f59142bf32d75026fc1162babee5e76c4
TuTran <minhtu.tran.09041999@gmail.com>
2020-12-20 22:16:05 +0700

    add sign up function

--- common/common.php
+++ common/common.php
@@ -1,7 +1,6 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "", "assignment")
+    $con = mysqli_connect("localhost", "xyz", "9499", "ecommerce")
     or die(mysqli_error($con));
     if(!isset($_SESSION)){
       session_start();
     }
-?>
