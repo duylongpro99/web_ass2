bef2e6093b9bc39892b055c40d8f07df72ccc7f0
DELL <21AK22@DELL.DELL>
2020-12-23 08:50:51 +0700

    asjkd

--- login_submit.php
+++ login_submit.php
@@ -11,7 +11,6 @@ $email = $_POST['e-mail'];
 $password = $_POST['password'];
 $checkUser  = new userObj(0, '', $password, '', $email,'' ,'' );
 $data = $userController->checkInLogin($checkUser);
-
 if($data != null){
   $loginResponse = new checkLoginResponse($data["userId"], $data["userName"], $data["roleName"], $data["roleId"], $data["password"], $data["email"]
           , $data["contact"], $data["city"],$data["address"]);
