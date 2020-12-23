c78b5bb7971bed0fd09bd6f26ac4ffbf5eeb485c
DELL <21AK22@DELL.DELL>
2020-12-23 09:11:33 +0700

    daskjdsn

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    if(!isset($_SESSION['roleName']) ){
-        echo $_SESSION['roleName'];
+    if(isset($_SESSION['roleName']) ){
+        echo "you are admin";
     }
     else{
         echo "YOu are not admin,";
